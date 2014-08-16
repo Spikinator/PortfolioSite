18ddd1bd52cfe3e109f022eb026cfdc5f5ed3e10
Spikinator <spikeyspikesspikedspike@gmail.com>
2014-08-16 19:21:38 +0200

    Added folders

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+
+/*$(document).ready(function() {
+	console.log("JQuery Works!");
+	$('li').hover(function(){
+        //$(this).toggleClass("down");
+        $(this).slideDown();
+    });
+	
+});
+*/
